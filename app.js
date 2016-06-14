c554076aae1685f9b0fe14d067d5c34a5fc57edd
Peter Lynn <github@fantutte.net>
2016-06-14 15:49:34 -0400

    switch seats

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require( 'express' );
 var swig=require('swig');
+var routes = require("./routes");
+
+
 var app = express(); // creates an instance of an express application
 app.engine('html',swig.renderFile);
 app.set('view engine','html');
@@ -7,28 +10,31 @@ app.set('views',__dirname + '/views');
 swig.setDefaults({ cache: false });
 
 
-app.use(function(req, res, next) {
-	console.log("Request with method: ", req.method, "and path", req.path);
-	next();
-});
+// app.use(function(req, res, next) {
+// 	console.log("Request with method: ", req.method, "and path", req.path);
+// 	next();
+// });
 
-app.get("/", function(req, res, next) {
 
-var locals = {
-    title: 'An Example',
-    people: [
-        { name: 'Gandalf'},
-        { name: 'Frodo' },
-        { name: 'Hermione'}
-    ]
-};
-swig.renderFile(__dirname + '/views/index.html', locals, function (err, output) {
-    console.log(output);
-});
+app.use('/', routes);
 
-	//res.send("Welcome to our tiny server!");
-	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-res.render( 'index', {title: 'Hall of Fame', people: people} );
-})
+// app.get("/", function(req, res, next) {
+//
+// var locals = {
+//     title: 'An Example',
+//     people: [
+//         { name: 'Gandalf'},
+//         { name: 'Frodo' },
+//         { name: 'Hermione'}
+//     ]
+// };
+// swig.renderFile(__dirname + '/views/index.html', locals, function (err, output) {
+//     console.log(output);
+// });
+
+// 	//res.send("Welcome to our tiny server!");
+// 	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+// res.render( 'index', {title: 'Hall of Fame', people: people} );
+// })
 
 app.listen(3000);
